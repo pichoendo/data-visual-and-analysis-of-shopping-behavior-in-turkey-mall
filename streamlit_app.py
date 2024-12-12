625b64a4a73dd2a2777071d0b162c44febd8d369
pichoendo <136944605+pichoendo@users.noreply.github.com>
2024-12-12 10:25:05 +0000

    update icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Streamlit Title
-st.title("Analyzing Shopping Behavior in Istanbul Mall")
+st.title("👜Analyzing Shopping Behavior in 🕌Istanbul Mall")
 
 # Load Data
 df = pd.read_csv('data/data.csv', usecols=[
